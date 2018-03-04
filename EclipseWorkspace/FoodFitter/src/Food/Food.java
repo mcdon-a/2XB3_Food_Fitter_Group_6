60640aa40281681d254140e1d0bf1c6183d4ea9c
thomas-armena <armenat@mcmaster.ca>
2018-03-04 18:06:19 -0500

    changes to food ADT

--- EclipseWorkspace/FoodFitter/src/Food/Food.java
+++ EclipseWorkspace/FoodFitter/src/Food/Food.java
@@ -1,17 +1,43 @@
 package Food;
 
+/**
+ * The Food class is an ADT that represents a food item by its id, descriptor, and nutritional values.
+ * It accepts 3 arguments; the id (int), the descriptor (String of tags separated by commas), 
+ * and the nutritional values (
+ * 
+ * Note that the inputed descriptor is converted to an array of its tags
+ * 
+ * @author Thomas Armena
+ *
+ */
+
 public class Food {
 	
-	private final int id;
-	private final String[] descriptor;
-	private final double[] nutrVals;
+	private final int id;						//stores the food ID
+	private final String[] descriptor;			//stores an array of tags that describe the food
+	private final double[] nutrVals;			//stores an array of nutritional values in respective order
 	
+	//constructor
 	public Food(int id, String descriptor, double[] nutrVals) {
 		this.id = id;
 		this.descriptor = descriptor.split(", ");
 		this.nutrVals = nutrVals;
 	}
 	
+	//GETTER methods
+	public int getID() {
+		return this.id;
+	}
+	
+	public String[] getDescriptor() {
+		return this.descriptor;
+	}
+	
+	public double[] nutrVals() {
+		return this.nutrVals;
+	}
+	
+	//String representation 
 	public String toString() {
 		String descriptor = "";
 		for(int i = 0; i < this.descriptor.length; i++) {
@@ -19,19 +45,26 @@ public class Food {
 			if (i < this.descriptor.length-1)
 				descriptor += ", ";
 		}
-		String natrVals = "";
+		String nutrVals = "";
 		for(int i = 0; i < this.nutrVals.length; i++) {
-			natrVals += ", ";
+			nutrVals += Double.toString(this.nutrVals[i]);
+			if (i < this.nutrVals.length - 1)
+				nutrVals += ", ";
 		}
-		return "{ (Food id: "+ this.id + "), (Descriptor: " + descriptor + "),(Nutritional Values: " + natrVals + ")}";
+		return "{ (Food id: "+ this.id + "), (Descriptor: " + descriptor + "),(Nutritional Values: " + nutrVals + ")}";
 	}
 	
+	//Temporary main to test code
 	public static void main(String[] args) {
+		//Creating a test food item
 		int id = 400078381;
 		String descriptor = "Fast foods, sandwiches and burgers, hamburger, regular, single patty, plain";
 		double[] nutrVals = {1,2,3,4,5,6,7,8};
 		Food food1 = new Food(id,descriptor,nutrVals);
+		
+		//Testing the outputs
 		System.out.println(food1);
+		System.out.println(food1.getDescriptor()[1]);
 	}
 
 }
