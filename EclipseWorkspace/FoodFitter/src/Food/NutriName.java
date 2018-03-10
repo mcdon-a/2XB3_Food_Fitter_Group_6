8fe461dde80fcd65eb9674b1b62fd1bcbe686637
Wyatt Wismer <crazy123frog@gmail.com>
2018-03-09 23:27:51 -0500

    Build nutrient lookup

---
+++ EclipseWorkspace/FoodFitter/src/Food/NutriName.java
@@ -0,0 +1,45 @@
+package Food;
+
+public class NutriName {
+	private final int NutrientCode;
+	private final String NutrientSymbol;
+	private final String NutrientUnit;
+	private final String NutrientName;
+	private final String NutrientNameF;
+	private final String Tagname;
+	private final int NutrientDecimals;
+	
+	
+	public NutriName(int code, String symbol, String unit, 
+			String name,String nameF,String tagname,int decimals){
+		NutrientCode = code;
+		NutrientSymbol = symbol;
+		NutrientUnit = unit;
+		NutrientName = name;
+		NutrientNameF = nameF;
+		Tagname = tagname;
+		NutrientDecimals = decimals;
+	}
+	
+	public int getCode() {
+		return NutrientCode;
+	}
+	public String getSymbol() {
+		return NutrientSymbol;
+	}
+	public String getUnit() {
+		return NutrientUnit;
+	}
+	public String getName() {
+		return NutrientName;
+	}
+	public String getNameF() {
+		return NutrientNameF;
+	}
+	public String getTagname() {
+		return Tagname;
+	}
+	public int getDecimals() {
+		return NutrientDecimals;
+	}
+}
