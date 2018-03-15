4ab638a377bdfe1f05596c17b670209bf573c4eb
inksplatt <30760803+sophiatao@users.noreply.github.com>
2018-03-15 12:20:37 -0400

    Tomcat POST

--- tomcat/webapps/hello/WEB-INF/classes/HelloServlet.java
+++ tomcat/webapps/hello/WEB-INF/classes/HelloServlet.java
@@ -20,9 +20,13 @@ public class HelloServlet extends HttpServlet {
          out.println("<h1>Hello, world!</h1>");  // says Hello
          // Echo client's request information
          out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+         out.println("<p>Request Header: " + request.getHeaderNames() + "</p>");
+         out.println("<p>Request Method: " + request.getMethod() + "</p>");
          out.println("<p>Protocol: " + request.getProtocol() + "</p>");
          out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
          out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+         out.println("<p>Username: " + request.getParameter("username") + "</p>");
+         out.println("<p>Password: " + request.getParameter("password") + "</p>");
          // Generate a random number upon each request
          out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
          out.println("</body></html>");
@@ -30,4 +34,23 @@ public class HelloServlet extends HttpServlet {
          out.close();  // Always close the output writer
       }
    }
-}
+  @Override
+          public void doPost(HttpServletRequest request,
+                            HttpServletResponse response)
+              throws IOException, ServletException {
+      doGet(request, response);
+         String user = request.getParameter("username");
+         String pass = request.getParameter("password");
+         // User user = userService.find(username, password);
+         // System.out.println(user + pass);
+
+         // if (user != null) {
+         //     request.getSession().setAttribute("user", user);
+         //     response.sendRedirect("home");
+         // }
+         // else {
+         //     request.setAttribute("error", "Unknown user, please try again");
+         //     request.getRequestDispatcher("/login.jsp").forward(request, response);
+         // }
+      }
+      }
