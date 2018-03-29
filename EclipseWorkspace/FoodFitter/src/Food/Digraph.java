1a20e061df10e778cdf14094dfb2294a9adaa0b6
unknown <opaillya@gmail.com>
2018-03-28 22:36:04 -0400

    Created file for group graphs, and implemented Digraph and import to create the digraph from the file.

---
+++ EclipseWorkspace/FoodFitter/src/Food/Digraph.java
@@ -0,0 +1,64 @@
+package Food;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+import Food.GroupInfo;
+
+public class Digraph {
+	private final int V;
+	private int E = 0;
+	private ArrayList<Integer>[] adj;
+	int[] indegree;
+
+	public Digraph() {
+        this.V = GroupInfo.GROUP_AMOUNT;
+        this.E = 0;
+        indegree = new int[V];
+        adj = (ArrayList<Integer>[]) new ArrayList[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new ArrayList<Integer>();
+        }
+    }
+
+
+	public int V() {
+		return V;
+	}
+
+	public int E() {
+		return E;
+	}
+
+
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+
+
+	public int outdegree(int v) {
+		return adj[v].size();
+	}
+
+
+	public int indegree(int v) {
+		return indegree[v];
+	}
+
+	
+	public Digraph reverse() {
+		Digraph reverse = new Digraph();
+		for (int v = 0; v < V; v++) {
+			for (int w : adj(v)) {
+				reverse.addEdge(w, v);
+			}
+		}
+		return reverse;
+	}
+	
+	public void addEdge(int v, int w) {
+        adj[v].add(w);
+        indegree[w]++;
+        E++;
+    }
+}
