257d1265518d544f10503083986883a6ff57390c
Illya Pilipenko <pilipeni@mcmaster.ca>
2018-04-10 18:09:37 -0400

    Modified group info

--- EclipseWorkspace/FoodFitter/src/Food/GroupInfo.java
+++ EclipseWorkspace/FoodFitter/src/Food/GroupInfo.java
@@ -4,12 +4,47 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 
+/**
+ * Class for importing the information for food groups from the CSV files
+ *
+ * @author Illya Pilipenko
+ *         <p>
+ *         Date: 2018 - 03 - 28
+ */
 public class GroupInfo {
+	/**
+	 * The filepath to the FOOD GROUP.csv file.
+	 */
 	private static final String FOOD_GROUP_PATH = "data\\FOOD GROUP.csv";
+	/**
+	* The row width in the csv files
+	*/
 	private static final int ROW_WIDTH = 4;
-	public static final int GROUP_AMOUNT = 26;
+	/**
+	* The hashmap containing all of the GroupName objects
+	*/
 	private static HashMap<Integer, GroupName> group_lookup = new HashMap<Integer, GroupName>();
 
+	/**
+	 * Method that takes a food object and its food ID, and then adds all of the
+	 * nutrient IDs and their corresponding amounts for said food from the Nutrient
+	 * Amount.csv file.
+	 *
+	 * @param read
+	 *            BufferedReader object that is reading from the Nutrient Amount.csv
+	 *            file; assumed to start at the 2nd nutrient for the current food.
+	 * @param firstLine
+	 *            The 1st nutrient for the current food. This is needed since the
+	 *            1st line of the next food's nutrients needs to be read to make
+	 *            sure there are no more nutrients remaining for the current food.
+	 * @param food
+	 *            The Food object to which to add nutrients.
+	 * @param foodID
+	 *            The Food ID of the current Food object; used to check that the
+	 *            nutrients being added are for the right food.
+	 * @return returns This method returns the 1st line of the next food's nutrients
+	 *         so that it may be passed to the next food's addNutrients pass.
+	 */
 	public static void init_info() {
 		// Define buffered reader
 		BufferedReader br = null;
