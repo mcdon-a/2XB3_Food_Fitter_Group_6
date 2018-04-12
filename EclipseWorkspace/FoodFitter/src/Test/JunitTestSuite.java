68b51f048cfa2ad877e0d8d9baaf0d9078c50d81
Alexie McDonald <alexiemcdonald304@gmail.com>
2018-04-11 20:09:28 -0400

    More tests again

---
+++ EclipseWorkspace/FoodFitter/src/Test/JunitTestSuite.java
@@ -0,0 +1,24 @@
+package Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   FitterTest.class,
+   FoodTest.class,
+   GroupInfoTest.class,
+   GroupNameTest.class,
+   InsertionTest.class,
+   KnapsackTest.class,
+   MealTest.class,
+   NutrientInfoTest.class,
+   NutrientTargetTest.class,
+   NutriNameTest.class,
+   PrefixTreeTest.class,
+   ReadCSVTest.class,
+   TimSortTest.class
+})
+
+public class JunitTestSuite {   
+}  
--- EclipseWorkspace/FoodFitter/src/Test/TimSortTest.java
+++ EclipseWorkspace/FoodFitter/src/Test/TimSortTest.java
@@ -20,16 +20,13 @@ public class TimSortTest {
 		for (int i = 0; i < ARRAY_SIZE; i++) {
 			toSort[i] = new Food(new String[] {});
 			toSort[i].addNutr(NUTR_ID, (int)(Math.random()*1000));
-			System.out.println(toSort[i].getNutr(NUTR_ID));
 		}
 		TimSort.sortMerge(toSort, NUTR_ID);
-		System.out.println();
 	}
 
 	@Test
 	public void testSortMerge() {
 		for (int i = 0; i < ARRAY_SIZE - 1; i++) {
-			System.out.println(toSort[i].getNutr(NUTR_ID));
 			assertTrue(toSort[i].getNutr(NUTR_ID) <= toSort[i+1].getNutr(NUTR_ID));
 		}
 	}
